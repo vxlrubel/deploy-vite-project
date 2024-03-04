f7300b78875ae4663cb082a5d5230735c140aa04
Rubel Mahmud <vxlrubel@gmail.com>
2024-03-05 01:31:16 +0600

    first commit

---
+++ src/views/ServicesView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="services">
+      <h1>This is Service page</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .services {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
