b25d3381acf5ad311a1f47f0d45ba61770ab6bdf
Rubel Mahmud <vxlrubel@gmail.com>
2024-03-05 01:57:30 +0600

    change config file

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-site/',
+  base: '/deploy-vite-project/',
   plugins: [
     vue(),
   ],
@@ -14,4 +14,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
